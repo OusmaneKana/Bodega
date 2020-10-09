dc6ef5fad2a2d2603b6a6144ea88ae137af15817
OusmaneKana <kanaman6@gmail.com>
2020-10-09 02:22:22 -0500

    Last update 10/8
    - Code update
    - DB update

--- bodega/bodega/items.py
+++ bodega/bodega/items.py
@@ -6,6 +6,8 @@
 import scrapy
 
 
+
+
 class BodegaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
--- bodega/bodega/spiders/bodega_spider.py
+++ bodega/bodega/spiders/bodega_spider.py
@@ -1,27 +1,49 @@
 import scrapy
 import pandas as pd
-import pandas as pd 
+import time
 
 
 data = pd.read_excel('C:\\Users\\Ousmane Kana\\Desktop\\Bodega\\sbler-Copy.xlsx', header=None)
 
-serials =data[0].tolist()
+ID =data[0].tolist()
 
-def LinkMaker(ID):
-    
-    link = "https://www.thdsalvage.com/Inventory/Detail?wid=8617&sbn="+str(ID)+"&wid=8617&sbn="+str(ID)
+
+def read_csv(name):
+	data = pd.read_csv(r'C:\Users\Ousmane Kana\Desktop\Bodega\dataBase\557223.csv')
+	Model_Numbers = data['Model Numbers'].values.tolist()
+	SKU =  data['SKU'].values.tolist()
+
+	return(SKU)
+
+
+with open(r'C:\Users\Ousmane Kana\Desktop\Bodega\dataBase\ID.txt', 'w') as f:
+    for item in ID:
+        f.write("%s\n" % item)
+
+def Wholesale_Link(item):
+   
+    link = "https://www.thdsalvage.com/Inventory/Detail?wid=8617&sbn="+str(item)+"&wid=8617&sbn="+str(item)
     return (link)
 
 
+def HD_Link(item):
+    
+    links= []
+    
+    for models in read_csv(ID):
+        link = ("https://www.homedepot.com/s/"+str(models))
+        links.append(link)
+
+    return(links)
 
 
+class Wholesale_Spider(scrapy.Spider):
 
-class BodegaSpider(scrapy.Spider):
-	name = "homedepot"
+	name = "wholesale"
 
 	allowed_domain = ["https://www.thdsalvage.com/"]
 
-	link = map(LinkMaker, serials)
+	link = map(Wholesale_Link, ID)
 
 	start_urls = list(link)
 
@@ -31,11 +53,15 @@ class BodegaSpider(scrapy.Spider):
 		# with open(filename, "wb") as f:
 		# 	f.write(response.body)
 
+
+
 		Category = response.xpath("//table[@style=\"text-align:right; width:100%\"]//td[1]//text()").extract()[8]
 
 		Warehouse = response.xpath("//table[@style=\"text-align:right; width:100%\"]//td[1]//text()").extract()[6]
 
 		Container_Qty = response.xpath("//table[@style=\"text-align:right; width:100%\"]//td[1]//text()").extract()[12]
+
+		SB = response.xpath("//table[@style=\"text-align:right; width:100%\"]//td[1]//text()").extract()[2].strip(" ;")
 
 		# Iterate through the table to get all the stuffs
 
@@ -51,19 +77,17 @@ class BodegaSpider(scrapy.Spider):
 
 		ItemsDf = pd.DataFrame(list(zip(SKU, Model_number)), columns =['SKU', "Model Numbers"])
 
+		ItemsDf.to_csv(r"C:\Users\Ousmane Kana\Desktop\Bodega\dataBase\\"+str(SB)+".csv", index = False, header=True)
+
 		print(ItemsDf.head())
 
 		#print("*****************************************")
-
-
-		
 		print("\nThe Category is {} is located in {} and the quantity of the container is {}\n\n".format(Category, Warehouse, Container_Qty))
 
+		
 
 		#print(SKU, Model_number)
-
 		print("*****************************************")
-
 
 		# price = response.xpath('//span[@class="price__dollars"]/text()').extract()
 		# name = response.xpath('//h1[@class="product-title__title"]/text()').extract()
@@ -72,3 +96,44 @@ class BodegaSpider(scrapy.Spider):
 		# print(f"The price is {price}")
 		# print(f"The name is {name}")
 		# print("\n\n\n\n********************************")
+
+class Homedepot_spider(scrapy.Spider):
+
+	name ="homedepot"
+
+
+	allowed_domain = ["www.homedepot.com"]
+
+	Links = map(HD_Link, ID)
+	Link = list(Links)
+	flat_list = [item for sublist in Link for item in sublist]
+	print(flat_list[0:5])
+
+	start_urls = flat_list[0:5]
+
+
+
+
+
+	
+
+
+	def parse(self,response):
+
+
+		price = response.xpath('//span[@class="price__dollars"]/text()').extract()
+		name = response.xpath('//h1[@class="product-title__title"]/text()').extract()
+
+		print("\n\n\n\n********************************")
+		print(response.url)
+		print(f"The price is {price}")
+		print(f"The name is {name}")
+		print("\n\n\n\n********************************")
+
+
+
+
+
+
+
+		
